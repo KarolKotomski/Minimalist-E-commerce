421c2eedeb473326b121872a0e37a9ddcdb4d77f
KarolKotomski <karol.kotomski88@gmail.com>
2023-08-05 20:04:28 +0200

    reversed banner introduced

--- src/components/BannerReverse.jsx
+++ src/components/BannerReverse.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Banner.css";
+import banner2 from "../images/banner/banner2.jpg";
 
 const BannerReverse = () => {
-	return <div>BannerReverse</div>;
+	return (
+		<div className='banner'>
+			<div className='container'>
+				<div className='banner-reverse-container'>
+					<div className='text-side'>
+						<div className='text'>
+							<h2>Comfortable and elegant living</h2>
+							<button>shop now</button>
+						</div>
+					</div>
+					<div className='img-side'>
+						<img src={banner2}></img>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default BannerReverse;
