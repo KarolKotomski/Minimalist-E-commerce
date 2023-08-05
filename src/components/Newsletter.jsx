e092dc7f55552fc5d6151e0d547e241d77ab6a75
KarolKotomski <karol.kotomski88@gmail.com>
2023-08-05 21:00:06 +0200

    newsletter field introduced

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "./Newsletter.css";
 
 const Newsletter = () => {
 	return (
 		<div className='newsletter'>
 			<div className='container'>
-				<div className='newsletter-container'>NEWSLETTER</div>
+				<div className='newsletter-container'>
+					<h2>Subscribe to the Newsletter!</h2>
+					<form>
+						<input type="email" placeholder="your@email.com" />
+						<button type="submit">Subscribe</button>
+					</form>
+				</div>
 			</div>
 		</div>
 	);
