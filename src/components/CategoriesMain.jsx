41f121b7f5c367371291c36e68e7cab08ff0134a
KarolKotomski <karol.kotomski88@gmail.com>
2023-08-10 13:34:54 +0200

    category page files created

---
+++ src/components/CategoriesMain.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "../components/CategoriesMain.css";
+import ProudItem from "../components/ProudItem";
+
+const CategoriesMain = () => {
+	return (
+		<div className='categories-main'>
+			<div className='proud'>
+				<div className='container'>
+					<div className='cat-main-container'>
+						<div className="cat-main-header">
+							<h1>select category:</h1>
+                        </div>
+                        <div className="filter-btns">
+                            <button>all products</button>
+                            <button>chairs</button>
+                            <button>electronics</button>
+                            <button>furnitures</button>
+                            <button>kitchen</button>
+                            <button>lamps</button>
+                            <button>skin care</button>
+                        </div>
+					</div>
+					<div className='proud-container'>
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+						<ProudItem />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CategoriesMain;
