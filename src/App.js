27f9cd325a2529f311de53774895c3eff4346670
KarolKotomski <karol.kotomski88@gmail.com>
2023-08-10 16:52:34 +0200

    page not found added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
 import Categories from "./pages/Categories";
+import NoMatch from "./components/NoMatch";
 
 const App = () => {
 	return (
@@ -13,6 +14,7 @@ const App = () => {
 				<Route path='/' element={<Home />} />
 				<Route path='categories' element={<Categories />} />
 				<Route path='productpage' element={<ProductPage />} />
+				<Route path='*' element={<NoMatch />} />
 			</Routes>
 		</>
 	);
---
+++ src/components/NoMatch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../components/NoMatch.css"
+
+const NoMatch = () => {
+  return (
+    <div className='pgnf'>page not found</div>
+  )
+}
+
+export default NoMatch
