2265ec8520d915b33cdb8ecf6a377be10236e8c0
KarolKotomski <karol.kotomski88@gmail.com>
2023-08-12 22:55:12 +0200

    slider functionality added

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -5,21 +5,31 @@ import arrowRight from "../images/icons/arrow-right.png";
 import TrendingItem from "./TrendingItem";
 
 const Trending = () => {
+	const slideRight = () => {
+		const slider = document.getElementById("slider");
+		slider.scrollLeft = slider.scrollLeft + 200;
+	};
+
+	const slideLeft = () => {
+		const slider = document.getElementById("slider");
+		slider.scrollLeft = slider.scrollLeft - 200;
+	};
+
 	return (
 		<div className='trending'>
 			<div className='container'>
 				<div className='title-btns'>
 					<h3>Trending now:</h3>
 					<div className='buttons'>
-						<button>
+						<button onClick={slideLeft}>
 							<img src={arrowLeft} alt='left-arrow' />
 						</button>
-						<button>
+						<button onClick={slideRight}>
 							<img src={arrowRight} alt='right-arrow' />
 						</button>
 					</div>
 				</div>
-				<div className='trending-container'>
+				<div className='trending-container' id='slider'>
 					<TrendingItem />
 					<TrendingItem />
 					<TrendingItem />
@@ -34,7 +44,6 @@ const Trending = () => {
 					<TrendingItem />
 					<TrendingItem />
 				</div>
-			
 			</div>
 		</div>
 	);
