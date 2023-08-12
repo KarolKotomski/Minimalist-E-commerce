ef59c08b99703f6fb783c8b7420fca0bdbc95d54
KarolKotomski <karol.kotomski88@gmail.com>
2023-08-12 20:45:56 +0200

    product page counter funcionality added

--- src/components/ProductPageMain.jsx
+++ src/components/ProductPageMain.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/ProductPage.css";
-import pic19 from "../images/products/19.jpg";
+import { items } from "../components/AllData";
 
 const ProductPageMain = () => {
+	const [image, setImage] = useState(items[0].img);
+	const [quantity, setQuantity] = useState(1);
+
+	const changeImage = (e) => {
+		setImage(e.target.src);
+	};
+
+	const decrease = () => {
+		if (quantity > 0) {
+			setQuantity(quantity - 1);
+		}
+	};
+
+	const increase = () => {
+		if (quantity >= 0) {
+			setQuantity(quantity + 1);
+		}
+	};
+
+	const calcPrice = (quantity) => {
+		return quantity * items[0].price;
+
+	}
+
 	return (
 		<>
 			<div className='product-page'>
@@ -11,29 +35,37 @@ const ProductPageMain = () => {
 						<div className='product-div'>
 							<div className='product-left-side'>
 								<div className='big-img'>
-									<img src={pic19} alt='comfy chair' />
+									<img src={image} alt='' />
 								</div>
 								<div className='small-imgs'>
-									<img src={pic19} alt='comfy chair1' />
-									<img src={pic19} alt='comfy chair2' />
-									<img src={pic19} alt='comfy chair3' />
+									<img src={items[0].img} alt='' onMouseOver={changeImage} />
+									<img
+										src={items[0].otherImgs[0]}
+										alt=''
+										onMouseOver={changeImage}
+									/>
+									<img
+										src={items[0].otherImgs[1]}
+										alt=''
+										onMouseOver={changeImage}
+									/>
 								</div>
 							</div>
 							<div className='product-right-side'>
-								<h3 className='title-header'>Comfy chair</h3>
-								<p className='product-spec'>description</p>
+								<h3 className='title-header'>{items[0].description}</h3>
+								<p className='product-spec'>{items[0].specs}</p>
 								<div className='product-quant'>
 									<div className='quant-container'>
 										<p className='quant-price-text'>Quantity</p>
 									</div>
 
 									<div className='quantity-btns'>
-										<button>-</button>
-										<p className='count-number'>0</p>
-										<button>+</button>
+										<button onClick={decrease}>-</button>
+										<p className='count-number'>{quantity}</p>
+										<button onClick={increase}>+</button>
 									</div>
 									<div className='price-container'>
-										<p className='quant-price-text'>900$</p>
+										<p className='quant-price-text'>{calcPrice(quantity)} $</p>
 									</div>
 								</div>
 								<div className='shopping-buttons'>
@@ -45,15 +77,15 @@ const ProductPageMain = () => {
 						<div className='specifications'>
 							<div className='spec'>
 								<p className='spec-type'>Texture:</p>
-								<p className='spec-text'>text</p>
+								<p className='spec-text'>{items[0].texture}</p>
 							</div>
 							<div className='spec'>
 								<p className='spec-type'>Weight:</p>
-								<p className='spec-text'>text</p>
+								<p className='spec-text'>{items[0].weight}</p>
 							</div>
 							<div className='spec'>
 								<p className='spec-type'>Size:</p>
-								<p className='spec-text'>text</p>
+								<p className='spec-text'>{items[0].size}</p>
 							</div>
 						</div>
 					</div>
