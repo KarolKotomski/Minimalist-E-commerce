fd93375700619923e4af500c571890a659b65ad3
KarolKotomski <karol.kotomski88@gmail.com>
2023-08-14 16:09:53 +0200

    header title correspondes the citem category

--- src/components/CategoriesHeader.jsx
+++ src/components/CategoriesHeader.jsx
@@ -4,34 +4,40 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const CategoriesHeader = () => {
+	const [title, setTitle] = useState("All Products");
+
+	const handleClick = (e) => {
+		setTitle(e);
+	};
+
 	return (
 		<div className='categories-main'>
 			<div className='proud'>
 				<div className='container'>
 					<div className='cat-main-container'>
 						<div className='cat-main-header'>
-							<h1>select category:</h1>
+							<h1>{title}</h1>
 						</div>
 						<div className='filter-btns'>
-							<Link to='all'>
+							<Link to='all' onClick={() => handleClick("all products")}>
 								<button>all products</button>
 							</Link>
-							<Link to='chairs'>
+							<Link to='chairs' onClick={() => handleClick("chairs")}>
 								<button>chairs</button>
 							</Link>
-							<Link to='electronics'>
+							<Link to='electronics' onClick={() => handleClick("electronics")}>
 								<button>electronics</button>
 							</Link>
-							<Link to='furnitures'>
+							<Link to='furnitures' onClick={() => handleClick("furnitures")}>
 								<button>furnitures</button>
 							</Link>
-							<Link to='kitchen'>
+							<Link to='kitchen' onClick={() => handleClick("kitchen")}>
 								<button>kitchen</button>
 							</Link>
-							<Link to='lamps'>
+							<Link to='lamps' onClick={() => handleClick("lamps")}>
 								<button>lamps</button>
 							</Link>
-							<Link to='skin-care'>
+							<Link to='skin-care' onClick={() => handleClick("skin care")}>
 								<button>skin care</button>
 							</Link>
 						</div>
